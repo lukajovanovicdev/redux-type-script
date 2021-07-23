d1f45715c7038203064a62099099c0da586b7a2c
LUKA JOVANOVIC <lukajovanovicdev@gmail.com>
2021-07-23 14:10:56 +0200

    Display data

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -8,7 +8,7 @@ export const searchRepositories = (term: string) => {
     dispatch({ type: ActionType.SEARCH_REPOSITORIES });
 
     try {
-      const { data } = await axios.get('https://reqistry.npmjs.org/-/v1/search', {
+      const { data } = await axios.get('http://registry.npmjs.com/-/v1/search', {
         params: { text: term },
       });
 
