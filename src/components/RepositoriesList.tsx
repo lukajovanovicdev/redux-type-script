13a570d276d9d74e39684c6f72036c502a4b97a6
LUKA JOVANOVIC <lukajovanovicdev@gmail.com>
2021-07-23 13:47:59 +0200

    Make the useActions hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useActions } from '../hooks/useActions';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector((state) => state.repositories);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
